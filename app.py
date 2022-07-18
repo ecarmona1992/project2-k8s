df1585e3e0d18f5d1fd6b4a3d603d8c428092b8f
OthomDev <107086987+OthomDev@users.noreply.github.com>
2022-07-18 19:39:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 import os
 @app.route('/')
 def hello():
-    return 'Hello appv again green !  \n'
+    return 'Hello flask blue !  \n'
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
