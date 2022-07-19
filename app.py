3ce69de679b2948c2e6572d8c99f2ad0d290c535
ecarmona1992 <emanuelcarmona714@gmail.com>
2022-07-19 09:07:53 -0700

    new test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 import os
 @app.route('/')
 def hello():
-    return 'This is a new test for the green Environment V2  \n'
+    return 'Green Test Environment!\n'
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
